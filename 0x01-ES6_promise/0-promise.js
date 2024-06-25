c0ddc5e3f9a9ba0debb06801964ed0669d5b8fbd
Desheye <aarealaafiaboyz@gmail.com>
2024-06-25 18:53:06 +0000

    1-promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,14 +1,12 @@
-// 0-promise.js
 export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        // Simulate an asynchronous operation, e.g., an API call
-        setTimeout(() => {
-            const success = true; // Change this to false to simulate a failure
-            if (success) {
-                resolve("API response data");
-            } else {
-                reject("Error occurred");
-            }
-        }, 1000); // Simulate a 1-second delay
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const success = true;
+      if (success) {
+        resolve('API response data');
+      } else {
+        reject(new Error('Error occurred'));
+      }
+    }, 1000);
+  });
 }
