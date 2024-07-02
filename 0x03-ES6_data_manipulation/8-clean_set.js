16fc3d74bed101113ca0886db5dca1ef66f140d7
Desheye <aarealaafiaboyz@gmail.com>
2024-07-02 07:21:54 +0000

    modified 8 clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,19 +1,13 @@
-/**
- * Cleans a set by filtering and transforming its values.
- * @param {Set} set - The set to clean.
- * @param {string} startString - The string to check at the start of each value.
- * @returns {string} A string of filtered and transformed set values, joined by '-'.
- */
-function cleanSet(set, startString) {
-  if (typeof startString !== 'string' || startString === '') {
-    return '';
-  }
+const cleanSet = (set, startString) => {
+  const strings = [];
 
-  const filteredValues = [...set]
-    .filter((value) => typeof value === 'string' && value.startsWith(startString))
-    .map((value) => value.slice(startString.length));
-
-  return filteredValues.join('-');
-}
+  if (startString === '' || typeof startString !== 'string') return '';
+  set.forEach((s) => {
+    if (typeof s === 'string' && s.startsWith(startString)) {
+      strings.push(s.slice(startString.length));
+    }
+  });
+  return strings.join('-');
+};
 
 export default cleanSet;
