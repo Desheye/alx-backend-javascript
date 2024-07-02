25f477b0c8f2770b55bb81290e6bcca98060a516
Desheye <aarealaafiaboyz@gmail.com>
2024-07-02 07:35:33 +0000

    6 set modified

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,11 +1,10 @@
-/* eslint-disable */
 /**
  * Creates a Set from an array.
  * @param {Array} arr - The input array of any kind of elements.
  * @returns {Set} A new Set created from the input array.
  */
+
 function setFromArray(arr) {
   return new Set(arr);
 }
-
 export default setFromArray;
